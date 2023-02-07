1d014787d6023c231483aa30959d158c12da08a2
Victor Senna <vhsenna@gmail.com>
2023-02-06 23:30:02 -0300

    Creating the structure of the app

--- StravaMap/urls.py
+++ StravaMap/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from .views import *
 
 urlpatterns = [
-
+    path('', base_map, name='Base Map View'),
+    path('oauth/', include('social_django.urls', namespace='social')),
 ]
--- StravaMap/views.py
+++ StravaMap/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+import folium
 
 # Create your views here.
+def base_map(request):
+    # Make your map object
+    main_map = folium.Map(location=[-8.0259, -34.9107], zoom_start=12) # Create base map
+    main_map_html = main_map._repr_html_() # Get HTML for website
+
+    context = {
+        'main_map': main_map_html
+    }
+    return render(request, 'index.html', context)
